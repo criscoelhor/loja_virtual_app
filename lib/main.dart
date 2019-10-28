b2c70c01de135f4d6b4220e70c48e5d43c481acb
Cristiano Coelho Ribeiro <cristiano.ribeiro@mynarski.com>
2019-10-28 17:59:48 -0300

    corrigindo bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja Flutter',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
-    
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
     );
   }
 }
