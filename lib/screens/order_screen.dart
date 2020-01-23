9c23237e672d8275bb23ecc9b5d732ad5365ab71
criscoelhor <coelhocr@gmail.com>
2020-01-22 23:11:17 -0300

    add confirm pedido

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class OrderScreen extends StatelessWidget {
-
   final String orderId;
 
   OrderScreen(this.orderId);
@@ -9,15 +8,26 @@ class OrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Pedido Realizado"),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-
-
-      )
-    );
+        appBar: AppBar(
+          title: Text("Pedido Realizado"),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.check,
+                color: Theme.of(context).primaryColor,
+                size: 80.0
+              ),
+              Text("Pedido realizado com sucesso!",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+              ),
+              Text("Có")
+            ],
+          ),
+        ));
   }
 }
